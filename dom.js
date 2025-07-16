4f1072aa603c6fc13cb4c966a22cd6f522bd1a42
John Doe <johndoe@example.com>
2025-07-16 16:20:37 +0200

    seconde session

---
+++ dom.js
@@ -0,0 +1,4 @@
+// Sélectionne le paragraphe d’id intro et change son texte.
+document.getElementById("intro").innerHTML = "nouvelle intro"
+// Ajoute la classe important à un élément avec la classe note.
+document.querySelector(".note").classList.add("important")
